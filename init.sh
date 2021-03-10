826ec558c794082b6fa11628a098d4df3f113bd4
Devin Don <i.inf@outlook.com>
2021-03-10 11:28:00 +0800

    init

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+mkdir -p $1/template
+mkdir -p $1/standard
+mkdir -p $1/wrong_answer
+mkdir -p $1/runtime_error
+django-admin startproject app $1/template
+
+cd $1/template
+django-admin startapp students
