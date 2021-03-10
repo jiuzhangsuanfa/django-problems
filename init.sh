a1439f5dc02f36bec3e7a08a9cb56bde3a5b55af
Devin Don <i.inf@outlook.com>
2021-03-10 13:29:14 +0800

    perf: rename template project

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-mkdir -p $1/template
-mkdir -p $1/standard
-mkdir -p $1/wrong_answer
-mkdir -p $1/runtime_error
-django-admin startproject app $1/template
+# mkdir -p $1/template
+# mkdir -p $1/standard
+# mkdir -p $1/wrong_answer
+# mkdir -p $1/runtime_error
+# django-admin startproject app $1/template
 
-cd $1/template
-django-admin startapp students
+# cd $1/template
+# django-admin startapp students
+
+cp -r 000-project-template $1
